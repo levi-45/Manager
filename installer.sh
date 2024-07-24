3f8c790b31f9c73d6c1fe00942be280ce327a572
levi-45 <levi45@hotmail.co.uk>
2024-07-24 22:38:53 +0100

    24-7-24

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## setup command=wget -q "--no-check-certificate" https://github.com/levi-45/Manager/main/installer.sh -O - | /bin/sh
+## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/levi-45/Manager/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
 version='2.3'
 changelog='Fix Utils, Screen'
