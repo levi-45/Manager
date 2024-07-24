abadd9199d91c314e29d5c0372527bcf4d234710
levi-45 <levi45@hotmail.co.uk>
2024-07-24 22:58:47 +0100

    24-7-24

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/levi-45/Manager/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
-version='2.3'
+version='10.1-r22'
 changelog='Fix Utils, Screen'
 ##
 TMPPATH=/tmp/Manager-main
