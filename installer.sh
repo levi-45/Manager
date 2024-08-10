3ef7cb874f826cbf6a433618296952d8f2dcb9ac
levi-45 <levi45@hotmail.co.uk>
2024-08-10 22:53:40 +0100

    .

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 ## setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/levi-45/Manager/main/installer.sh -O - | /bin/sh
 ## Only This 2 lines to edit with new version ######
 version='10.1-r27'
-changelog='Fixed Utils, Data, Update, Screen, and More'
+changelog='Fixed Many issues'
 ##
 TMPPATH=/tmp/Manager-main
 FILEPATH=/tmp/main.tar.gz
